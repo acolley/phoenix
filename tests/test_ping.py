3610ce2afcd75ec254c165fc038a9fdbe78638e5
Alasdair <alnessy@hotmail.com>
2020-07-09 16:26:27 +0100

    Add simple ping test

---
+++ tests/test_ping.py
@@ -0,0 +1,31 @@
+import pytest
+
+from phoenix import behaviour
+from phoenix.ref import Ref
+from phoenix.testing.kit import ActorTestKit
+
+
+class Ping:
+    @staticmethod
+    def start() -> behaviour.Behaviour:
+        async def recv(msg):
+            m, reply_to = msg
+            await reply_to.tell(m)
+            return behaviour.stop()
+        return behaviour.receive(recv)
+
+
+@pytest.fixture
+async def actor_test_kit():
+    kit = ActorTestKit()
+    await kit.run()
+    yield kit
+    await kit.shutdown()
+
+
+@pytest.mark.asyncio
+async def test_ping(actor_test_kit: ActorTestKit):
+    ping = await actor_test_kit.spawn(Ping.start)
+    probe = await actor_test_kit.create_probe()
+    await ping.tell(("Hello", probe.ref))
+    assert await probe.expect_message("Hello")
