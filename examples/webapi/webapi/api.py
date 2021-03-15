c986579ff7c0246df173003671b4ef3b716950a6
Alasdair Colley <alnessy@hotmail.com>
2021-03-15 17:50:46 -0400

    Refactor webapi example

--- examples/webapi/webapi/api.py
+++ examples/webapi/webapi/api.py
@@ -6,71 +6,74 @@ from functools import partial
 import logging
 import sys
 
-from phoenix import ActorId, ActorSystem, RestartStrategy, Router, Supervisor
+from phoenix import router
+from phoenix.actor import ActorId, Context
+from phoenix.dataclasses import dataclass
+from phoenix.supervisor import RestartStrategy, Supervisor
+from phoenix.system.system import ActorSystem
 
-from webapi.handler import RequestHandler
+from webapi import handler
 
 
-@attr.s
+@dataclass
+class State:
+    runner: web.AppRunner = attr.ib(validator=instance_of(web.AppRunner))
+    router_id: ActorId = attr.ib(validator=instance_of(ActorId))
+
+
+async def start(context: Context, host: str, port: int) -> State:
+    router_id = await context.spawn(
+        partial(
+            router.start,
+            workers=4,
+            start=handler.start,
+        ),
+        name="HttpApi.RequestHandlers",
+    )
+    context.link(context.actor_id, router_id)
+
+    async def hello(request):
+        return await context.call(
+            router_id, partial(handler.Hello, request=request)
+        )
+
+    app = web.Application()
+    app.add_routes([web.get("/", hello)])
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, host, port)
+    await site.start()
+    return State(runner=runner, router_id=router_id)
+
+
+async def handle(state: State, msg):
+    raise NotImplementedError
+
+
+async def cleanup(state: State):
+    pass
+
+
+@dataclass
 class HttpApi:
     actor_id: ActorId = attr.ib(validator=instance_of(ActorId))
     context = attr.ib()
 
-    @attr.s
-    class State:
-        runner: web.AppRunner = attr.ib(validator=instance_of(web.AppRunner))
-        router_id: ActorId = attr.ib(validator=instance_of(ActorId))
-
     @classmethod
     async def new(cls, context, host: str, port: int, name=None) -> "HttpApi":
         actor_id = await context.spawn(
-            partial(cls.start, host=host, port=port), cls.handle, name=name
+            partial(start, host=host, port=port), name=name
         )
         return cls(actor_id=actor_id, context=context)
-
-    @staticmethod
-    async def start(context, host: str, port: int) -> State:
-        router_id = await context.spawn(
-            partial(
-                Router.start,
-                workers=4,
-                start=RequestHandler.start,
-                handle=RequestHandler.handle,
-            ),
-            Router.handle,
-            name="HttpApi.RequestHandlers",
-        )
-        context.link(context.actor_id, router_id)
-
-        async def hello(request):
-            return await context.call(
-                router_id, partial(RequestHandler.Hello, request=request)
-            )
-
-        app = web.Application()
-        app.add_routes([web.get("/", hello)])
-        runner = web.AppRunner(app)
-        await runner.setup()
-        site = web.TCPSite(runner, host, port)
-        await site.start()
-        return HttpApi.State(runner=runner, router_id=router_id)
-
-    @staticmethod
-    async def handle(state: State, msg):
-        raise NotImplementedError
-
-    @staticmethod
-    async def cleanup(state: State):
-        pass
 
 
 async def main_async():
     system = ActorSystem()
-    task = asyncio.create_task(system.run())
+    await system.start()
 
     api = await HttpApi.new(system, "localhost", 8080, name="HttpApi")
 
-    await asyncio.sleep(60)
+    await system.run_forever()
     await system.shutdown()
 
 
--- examples/webapi/webapi/handler.py
+++ examples/webapi/webapi/handler.py
@@ -2,42 +2,43 @@ from aiohttp.web import Request, Response
 import attr
 from attr.validators import instance_of
 from functools import partial
-from multipledispatch import dispatch
 from typing import Tuple
 
-from phoenix import ActorId, Behaviour
+from phoenix.actor import Actor, ActorId, Behaviour, Context
+from phoenix.dataclasses import dataclass
 
 
-@attr.s
+@dataclass
+class State:
+    context: Context
+
+
+@dataclass
+class Hello:
+    reply_to: ActorId
+    request: Request
+
+
+async def start(context: Context) -> Actor:
+    return Actor(state=State(context=context), handler=handle)
+
+
+async def handle(state: State, msg: Hello) -> Tuple[Behaviour, State]:
+    await state.context.cast(msg.reply_to, Response(text="Hello world"))
+    return Behaviour.done, state
+
+
+@dataclass
 class RequestHandler:
-    actor_id: ActorId = attr.ib(validator=instance_of(ActorId))
-    context = attr.ib()
-
-    @attr.s
-    class State:
-        context = attr.ib()
-
-    @attr.s
-    class Hello:
-        reply_to: ActorId = attr.ib(validator=instance_of(ActorId))
-        request: Request = attr.ib(validator=instance_of(Request))
+    actor_id: ActorId
+    context: Context
 
     @classmethod
-    async def new(cls, context, name=None) -> "RequestHandler":
-        actor_id = await context.spawn(cls.start, cls.handle, name=name)
+    async def new(cls, context: Context, name=None) -> "RequestHandler":
+        actor_id = await context.spawn(start, name=name)
         return cls(actor_id=actor_id, context=context)
-
-    @staticmethod
-    async def start(context) -> State:
-        return RequestHandler.State(context=context)
-
-    @staticmethod
-    @dispatch(State, Hello)
-    async def handle(state: State, msg: Hello) -> Tuple[Behaviour, State]:
-        await state.context.cast(msg.reply_to, Response(text="Hello world"))
-        return Behaviour.done, state
 
     async def hello(self, request: Request) -> Response:
         return await self.context.call(
-            self.actor_id, partial(self.Hello, request=request)
+            self.actor_id, partial(Hello, request=request)
         )
