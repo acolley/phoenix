5d184a5ce76a30b9d1a46ee1a87930df091419bd
Alasdair Colley <alasdair@Alasdairs-MacBook-Pro.local>
2021-03-16 18:06:56 +0000

    Fix gauge example

--- examples/gauge/gauge.py
+++ examples/gauge/gauge.py
@@ -2,79 +2,85 @@ import asyncio
 import attr
 from attr.validators import instance_of
 import logging
-from multipledispatch import dispatch
+from multimethod import multimethod
 import sys
 from typing import Tuple
 
-from phoenix import (
+from phoenix.actor import (
+    Actor,
     ActorId,
-    ActorSystem,
     Behaviour,
+    Context,
     Down,
-    RestartStrategy,
-    Supervisor,
-    retry,
 )
+from phoenix.dataclasses import dataclass
+from phoenix.retry import retry
+from phoenix.supervisor import ChildSpec, RestartStrategy, RestartWhen, Supervisor
+from phoenix.system.system import ActorSystem
 
 
-@attr.s
+@dataclass
+class State:
+    ctx: Context
+    n: int
+
+
+@dataclass
+class Inc:
+    pass
+
+
+@dataclass
+class Dec:
+    pass
+
+
+@dataclass
+class Read:
+    reply_to: ActorId
+
+
+async def start(ctx) -> Actor:
+    return Actor(state=State(ctx, 0), handler=handle)
+
+
+@multimethod
+async def handle(state: State, msg: Inc) -> Tuple[Behaviour, State]:
+    raise ValueError
+    state.n += 1
+    return Behaviour.done, state
+
+
+@multimethod
+async def handle(state: State, msg: Dec) -> Tuple[Behaviour, State]:
+    state.n -= 1
+    return Behaviour.done, state
+
+
+@multimethod
+async def handle(state: State, msg: Read) -> Tuple[Behaviour, State]:
+    await state.ctx.cast(msg.reply_to, state.n)
+    return Behaviour.done, state
+
+
+@dataclass
 class Gauge:
-    actor_id: ActorId = attr.ib(validator=instance_of(ActorId))
-    ctx = attr.ib()
-
-    @attr.s
-    class State:
-        ctx = attr.ib()
-        n = attr.ib()
-
-    @attr.s
-    class Inc:
-        pass
-
-    @attr.s
-    class Dec:
-        pass
-
-    @attr.s
-    class Read:
-        reply_to: ActorId = attr.ib()
+    actor_id: ActorId
+    ctx: Context
 
     @classmethod
     async def new(cls, ctx, name=None) -> "Gauge":
-        actor_id = await ctx.spawn(cls.start, cls.handle, name=name)
+        actor_id = await ctx.spawn(start, name=name)
         return cls(ctx=ctx, actor_id=actor_id)
 
-    @staticmethod
-    async def start(ctx) -> State:
-        return Gauge.State(ctx, 0)
-
-    @staticmethod
-    @dispatch(State, Inc)
-    async def handle(state: State, msg: Inc) -> Tuple[Behaviour, State]:
-        raise ValueError
-        state.n += 1
-        return Behaviour.done, state
-
-    @staticmethod
-    @dispatch(State, Dec)
-    async def handle(state: State, msg: Dec) -> Tuple[Behaviour, State]:
-        state.n -= 1
-        return Behaviour.done, state
-
-    @staticmethod
-    @dispatch(State, Read)
-    async def handle(state: State, msg: Read) -> Tuple[Behaviour, State]:
-        await state.ctx.cast(msg.reply_to, state.n)
-        return Behaviour.done, state
-
     async def inc(self):
-        await self.ctx.cast(self.actor_id, self.Inc())
+        await self.ctx.cast(self.actor_id, Inc())
 
     async def dec(self):
-        await self.ctx.cast(self.actor_id, self.Dec())
+        await self.ctx.cast(self.actor_id, Dec())
 
     async def read(self) -> int:
-        return await self.ctx.call(self.actor_id, self.Read)
+        return await self.ctx.call(self.actor_id, Read)
 
 
 async def main_async():
@@ -86,10 +92,10 @@ async def main_async():
         name="Supervisor.Gauge",
     )
     await supervisor.init(
-        children=[(Gauge.start, Gauge.handle, dict(name="Gauge"))],
+        children=[ChildSpec(start=start, options=dict(name="Gauge"), restart_when=RestartWhen.permanent)],
         strategy=RestartStrategy.one_for_one,
     )
-    gauge = Gauge(actor_id=ActorId("Gauge"), ctx=system)
+    gauge = Gauge(actor_id=ActorId(system_id="system", value="Gauge"), ctx=system)
     await gauge.inc()
     await gauge.dec()
     # Timeout should be based on the average time it takes
